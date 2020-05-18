32b84f20ba3509e03311ab5e648964bcfeecede6
Francisco Giancarelli <francisco.giancarelli@4rsoluciones.com>
2020-05-18 00:49:26 -0300

    build para github pages

---
+++ build/precache-manifest.781e1e3f65576a8cdbe2b298fc6d3f7c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91df984e0119062d2277f151d4b1fba7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "750ad778faa8d35ac0c3",
+    "url": "/static/css/2.f2cef0e5.chunk.css"
+  },
+  {
+    "revision": "b53e5acc1778ea988154",
+    "url": "/static/css/main.5f367b49.chunk.css"
+  },
+  {
+    "revision": "750ad778faa8d35ac0c3",
+    "url": "/static/js/2.4345cbeb.chunk.js"
+  },
+  {
+    "revision": "f6f408a481ad675bb31883bfa37f34cb",
+    "url": "/static/js/2.4345cbeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b53e5acc1778ea988154",
+    "url": "/static/js/main.ebfe4177.chunk.js"
+  },
+  {
+    "revision": "23fde70cddf1aa14622a",
+    "url": "/static/js/runtime-main.526f71d9.js"
+  },
+  {
+    "revision": "012cf6a10129e2275d79d6adac7f3b02",
+    "url": "/static/media/MaterialIcons-Regular.012cf6a1.woff"
+  },
+  {
+    "revision": "570eb83859dc23dd0eec423a49e147fe",
+    "url": "/static/media/MaterialIcons-Regular.570eb838.woff2"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "/static/media/MaterialIcons-Regular.a37b0c01.ttf"
+  },
+  {
+    "revision": "e79bfd88537def476913f3ed52f4f4b3",
+    "url": "/static/media/MaterialIcons-Regular.e79bfd88.eot"
+  }
+]);
